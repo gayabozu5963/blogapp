d84944e6673410ea5df8f814176915dd269a148e
gayabozu5963 <forestone.5963@gmail.com>
2020-11-25 22:46:37 +0900

    update route

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,8 @@ Route::post('/store', 'BlogController@exeStore')->name('store');
 
 // ブログ詳細画面を表示
 Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
+
+// ブログ編集画面を表示
+Route::get('/blog/edit/{id}', 'BlogController@showEdit')->name('edit');
+
+Route::post('/blog/update', 'BlogController@exeUpdate')->name('update');
